998beddc54ca788d007555d226ad45de63946da4
Nitish Sharma <153273278+nitishsharma17@users.noreply.github.com>
2025-08-10 22:10:40 +0530

    Time: 1 ms (85.79%), Space: 40.7 MB (77.89%) - LeetHub

---
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean reorderedPowerOf2(int n) {
+        int[] input = countDigits(n);
+
+        for (int i = 0; i < 31; i++) {
+            int power = 1 << i;
+            int[] pArray = countDigits(power);
+
+            if (match(input, pArray)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean match(int[] a, int[] b) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        } 
+        return true;
+    }
+
+    private int[] countDigits(int n) {
+        int num = n;
+        int[] count = new int[10];
+
+        while (num > 0) {
+           count[num % 10] += 1; 
+           num = num / 10;
+        }
+
+        return count;
+    }
+}
