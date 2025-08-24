98e5acb920becb825d27530722a64886f13dfd9f
Nitish Sharma <153273278+nitishsharma17@users.noreply.github.com>
2025-08-25 01:23:16 +0530

    Time: 2 ms (96.58%), Space: 41.9 MB (47.84%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        if (s == null) return false;
+
+        Stack<Character> st = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') st.push(')');
+            else if (c == '{') st.push('}');
+            else if (c == '[') st.push(']');
+            else if (st.isEmpty() || st.pop() != c) return false;
+        }
+
+        return st.isEmpty();
+    }
+}
